dc0fc1c8e302a93b09951a39f980146a1a3a742a
James Bubb <jpbubb82@gmail.com>
2020-10-06 13:46:23 +0100

    Complete database section

--- server-upload.js
+++ server-upload.js
@@ -3,22 +3,41 @@ const multer = require('multer');
 const path = require('path');
 const uuid = require('uuid').v4;
 const app = express();
+const mongoose = require('mongoose');
+const Image = require('./models/Image');
+
+mongoose.connect('mongodb://127.0.0.1', { useNewUrlParser: true, useUnifiedTopology: true });
+// const connection = mongoose.connection;
+// connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
-        const { originalname } = file;
-        // or 
-        // uuid, or fieldname
-        cb(null, originalname);
+        const ext = path.extname(file.originalname);
+        const id = uuid();
+        const filePath = `images/${id}${ext}`;
+        Image.create({ filePath: filePath })
+            .then(() => {
+                cb(null, filePath)
+            });
     }
 })
 const upload = multer({ storage }); // or simply { dest: 'uploads/' }
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.static('uploads'));
 
 app.post('/upload', upload.array('avatar'), (req, res) => {
-    return res.json({ status: 'OK', uploaded: req.files.length });
+    return res.redirect('/');
+});
+
+app.get('/images', (req, res) => {
+    Image.find()
+        .then((images) => {
+            return res.json({ status: 'OK', images});
+        })
 });
 
 app.listen(3001);
